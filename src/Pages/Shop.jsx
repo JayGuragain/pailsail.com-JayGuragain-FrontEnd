99e96dadc41b96a049a9bc90922f62768fd0e0bb
JayGuragain <bijaya.uniglobe@gmail.com>
2024-01-28 21:46:14 -0800

    Fix build issues

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Hero from "../components/Hero/Hero";
-import Popular from "../components/Popular/Popular";
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
 import NewCollections from "../Components/NewCollections/NewCollections";
 import NewsLetter from "../Components/NewsLetter/NewsLetter";
-import Footer from "../Components/Footer/Footer";
 
 const Shop = () => {
   return (
